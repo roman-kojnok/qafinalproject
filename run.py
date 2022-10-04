0a4c30f7822a4387a496a7d8807b43685f76c80e
Roman Kojnok <roman.kojnok@protonmail.com>
2022-10-04 16:17:02 +0100

    Added run.py file

---
+++ run.py
@@ -0,0 +1,9 @@
+from main import create_app
+from main.blueprints.page import page
+from main.blueprints.page import calc
+
+if __name__ == '__main__':
+    app = create_app('flask.cfg')
+    app.register_blueprint(page)
+    app.register_blueprint(calc)
+    app.run(host='0.0.0.0')
